c23f15e588b3edde02354dc4cbdb69fbe54647ea
SaschaBorgesLucas <saschaborgeslucas@gmail.com>
2022-07-31 19:54:10 -0400

    Initial commit [0001-01]

---
+++ README.md
@@ -0,0 +1,3 @@
+# WaterAndSwimming
+Água e mecânicas de natação
+Developed with Unreal Engine 4
---
+++ Source/WaterAndSwimming/WaterAndSwimmingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WaterAndSwimmingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WATERANDSWIMMING_API AWaterAndSwimmingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
